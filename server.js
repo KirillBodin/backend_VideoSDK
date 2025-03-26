0a817c5100c1e18c71ef4fb5d6b250f1a2a9cb77
KirillBodin <kirillbodin2@gmail.com>
2025-03-26 18:30:36 +0200

    fix bug import

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"] }));
+app.use(cors({ origin: "https://meet.tamamat.com", credentials: true, methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"] }));
 app.use(express.json());
 
 
