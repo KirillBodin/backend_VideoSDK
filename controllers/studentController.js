5b4dfbee5de61cc897d2edaf797404ed6cf91d6a
KirillBodin <kirillbodin2@gmail.com>
2025-03-27 01:17:59 +0200

    fix check

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,34 +2,35 @@ import { Student, ClassMeeting } from "../models/index.js";
 import { Op } from "sequelize";
 
 export const checkStudentAccess = async (req, res) => {
-  const { slug	, email } = req.body;
+  const { slug, email } = req.body;
 
-  if (!slug	 || !email) {
-    return res.status(400).json({ error: "Missing meetingId or email" });
+  if (!slug || !email) {
+    return res.status(400).json({ error: "Missing slug or email" });
   }
 
   try {
-    
     const classMeeting = await ClassMeeting.findOne({ where: { slug } });
     if (!classMeeting) {
       return res.status(404).json({ error: "Lesson not found" });
     }
 
-  
     const student = await Student.findOne({ where: { email } });
     if (!student) {
       return res.status(404).json({ error: "Student not found" });
     }
 
-    
     const isRelated = await classMeeting.hasStudent(student);
 
-    return res.json({ access: isRelated });
+    return res.json({
+      access: isRelated,
+      meetingId: isRelated ? classMeeting.meetingId : null,
+    });
   } catch (err) {
     console.error("❌ Access check error:", err);
     return res.status(500).json({ error: "Server error" });
   }
 };
+
 
 export const getStudentsForTeacher = async (req, res) => {
   const { teacherId } = req.params;
