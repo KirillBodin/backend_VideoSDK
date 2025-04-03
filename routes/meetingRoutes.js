5cae47b4ccaeb374f8441557add07eeed11d3ff2
KirillBodin <kirillbodin2@gmail.com>
2025-04-03 12:50:01 +0300

    add getmeet

--- routes/meetingRoutes.js
+++ routes/meetingRoutes.js
@@ -19,7 +19,7 @@ router.post("/savemeeting/new", saveMeeting);
 router.get("/meet/:slug/:teacherName/:className", getMeetingBySlugTeacherNameClass);
 router.post("/meet/check-access", checkAccessToClass);
 router.get("/savemeeting/by-meetingid/:meetingId", getClassNameByMeetingId);
-router.get("/savemeeting/by-classname/:className", getMeetingByClassName);
+router.get("/getmeeting/by-classname/:className", getMeetingByClassName);
 router.post("/users/by-email", getUserRoleByEmail);
 
 export default router;
