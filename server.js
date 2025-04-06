4b1e49a6ee5776559697cc26d8ec260164fd8837
KirillBodin <kirillbodin2@gmail.com>
2025-04-06 22:53:56 +0300

    fix sever.js

--- server.js
+++ server.js
@@ -29,14 +29,36 @@ const PORT = process.env.PORT || 5000;
 
 app.set("trust proxy", 1);
 app.use(cookieParser());
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "https://meet.tamamat.com",
+  "https://t.me", 
+  "https://web.telegram.org", 
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 
 
 app.use("/api/admin", adminRoutes);
@@ -48,6 +70,14 @@ app.use("/api/school-admins", schoolAdminRoutes);
 app.use("/api/super-admin", superAdminRoutes);
 app.use("/api/student", studentRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error("❌ Global Error:", err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
 app.listen(PORT, async () => {
   await initDB();
